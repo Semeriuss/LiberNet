8901da67aab72b30d957d1a2557c68ca840e4cd1
Semere Habtu <se.semere.habtu@gmail.com>
2021-04-09 10:11:05 +0300

    Initial project and database setup

--- application.py
+++ application.py
@@ -5,6 +5,8 @@ from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+#set DATABASE_URL= "postgres://tmzgtmwfpuvzup:82d8da4476a90cf9f0acbc8ea68182cb85cbaba50dc31b77e8bcc960030b5e98@ec2-18-206-20-102.compute-1.amazonaws.com:5432/d3cbu5g4n0alq"
+
 app = Flask(__name__)
 
 # Check for environment variable
@@ -24,3 +26,7 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/")
 def index():
     return "Project One: TODO"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
